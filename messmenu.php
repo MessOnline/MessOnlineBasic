2d4c5d55cff9f88f957aa4831584bab411162f66
Aman Kumar <amankriet@gmail.com>
2018-05-16 21:01:37 +0530

    Add files via upload

---
+++ messmenu.php
@@ -0,0 +1,11 @@
+<?php include_once 'header.php'; ?>
+
+<section class="main-container">
+  <div class="main-wrapper">
+    <h2 class="pagetitle">Mess Menu</h2>
+  </div>
+
+  <img src="images/veg.jpg" alt="IMAGE">
+</section>
+
+<?php include_once 'footer.php'; ?>
